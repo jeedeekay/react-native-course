e39726cb92f42de5e39e9553e41054be6f6a2394
jeedeekay <gavinkinder@gmail.com>
2023-01-16 10:49:27 -0800

    Custom Side Drawer

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,13 +1,14 @@
-import { Platform, StyleSheet, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import DirectoryScreen from './DirectoryScreen';
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import { Icon } from 'react-native-elements';
+import logo from '../assets/images/logo.png';
 
 const Drawer = createDrawerNavigator();
 
@@ -30,15 +31,15 @@ const HomeNavigator = () => {
                         <Icon
                             name='home'
                             type='font-awesome'
-                            iconStyle={Styles.stackIcon}
+                            iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
                         />
                     )
                 })}
             />
         </Stack.Navigator>
-    )
-}
+    );
+};
 
 const DirectoryNavigator = () => {
     const Stack = createStackNavigator();
@@ -55,7 +56,7 @@ const DirectoryNavigator = () => {
                         <Icon
                             name='list'
                             type='font-awesome'
-                            iconStyle={Styles.stackIcon}
+                            iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
                         />
                     )
@@ -69,8 +70,8 @@ const DirectoryNavigator = () => {
                 })}
             />
         </Stack.Navigator>
-    )
-}
+    );
+};
 
 const AboutNavigator = () => {
     const Stack = createStackNavigator();
@@ -87,15 +88,15 @@ const AboutNavigator = () => {
                     <Icon
                         name='info-circle'
                         type='font-awesome'
-                        iconStyle={Styles.stackIcon}
+                        iconStyle={styles.stackIcon}
                         onPress={() => navigation.toggleDrawer()}
                     />
                 )
             })}
             />
         </Stack.Navigator>
-    )
-}
+    );
+};
 
 const ContactNavigator = () => {
     const Stack = createStackNavigator();
@@ -112,15 +113,29 @@ const ContactNavigator = () => {
                         <Icon
                             name='address-card'
                             type='font-awesome'
-                            iconStyle={Styles.stackIcon}
+                            iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
                         />
                     )
                 })}
             />
         </Stack.Navigator>
-    )
-}
+    );
+};
+
+const CustomDrawerContent = (props) => (
+    <DrawerContentScrollView {...props}>
+        <View style={styles.drawerHeader}>
+            <View style={{ flex: 1 }}>
+                <Image source={logo} style={styles.drawerImage} />
+            </View>
+            <View style={{ flex: 2 }}>
+                <Text style={styles.drawerHeaderText}>NuCamp</Text>
+            </View>
+        </View>
+        <DrawerItemList {...props} labelStyle={{ fontWeight: 'bold' }} />
+    </DrawerContentScrollView>
+)
 
 const Main = () => {
     return (
@@ -132,6 +147,7 @@ const Main = () => {
         >
             <Drawer.Navigator
                 initialRouteName='Home'
+                drawerContent={CustomDrawerContent}
                 drawerStyle={{ backgroundColor: '#CEC8FF' }}
             >
                 <Drawer.Screen
@@ -203,7 +219,25 @@ const Main = () => {
     );
 };
 
-const Styles = StyleSheet.create({
+const styles = StyleSheet.create({
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    },
     stackIcon: {
         marginLeft: 10,
         color: '#fff',
