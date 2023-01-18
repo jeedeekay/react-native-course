685a37157b9a511074c6d57511d874b39725fd3c
Gavin Kinder <gavinkinder@gmail.com>
2023-01-18 10:21:01 -0800

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,23 +2,59 @@ import { ScrollView,Text } from 'react-native';
 import { Card, ListItem, Avatar } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
+
+const Mission = () => {
+    return (
+        <Card>
+            <Card.Title>
+                Our Mission
+            </Card.Title>
+            <Card.Divider color='#fff0' />
+            <Text
+                style={{margin: 10}}
+            >
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
+    );
+}
 
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
 
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>
+                        Community Partners
+                    </Card.Title>
+                    <Card.Divider color='#fff0' />
+                    <Loading />
+                </Card>
+            </ScrollView>
+        )
+    }
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>
+                        Community Partners
+                    </Card.Title>
+                    <Card.Divider color='#fff0' />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        )
+    }
+
     return (
         <ScrollView>
-            <Card>
-                <Card.Title>
-                    Our Mission
-                </Card.Title>
-                <Card.Divider color='#fff0' />
-                <Text
-                    style={{margin: 10}}
-                >
-                    We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-                </Text>
-            </Card>
+            <Mission />
             <Card>
                 <Card.Title>
                     Community Partners
